ce00cc24f5cc3ebaa6c75d282f5619d24dc178aa
Karthikairasu K <karthikairasuk@gmail.com>
2020-09-15 18:52:51 +0530

    Update request_order_details.blade.php

---
+++ resources/views/inbound/request_order_details.blade.php
@@ -0,0 +1,39 @@
+<div class="container-fluid main_content bg-white p-2">
+        <div class="row mx-0">
+          <div class="col-12 text-center">
+            <h5 style="color: steelblue"> <b>Order Details</b> </h5>
+          </div>
+           
+          </div>
+          <div class="col-12 text-center">
+            <table id="example" class="table table-striped table-bordered example">
+              <thead>
+                  <tr class="bg_color">
+                      <th class="text-nowrap px-3">Item #</th>
+                      <th class="text-nowrap px-3">NUPCO Material</th>
+                      <th class="text-nowrap px-3">Customer Code</th>
+                      <th class="text-nowrap px-3">Description</th>
+                      <th class="text-nowrap px-3">UOM</th>
+                      <th class="text-nowrap px-3">Qty</th>
+                      <th class="text-nowrap px-3">Available</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>1</td>
+                      <td><input type="text" name="" value="material" readonly></td>
+                      <td><input type="text" name="" value="material" readonly></td>
+                      <td><input type="text" name="" value="material" readonly></td>
+                      <td><input type="text" name="" value="material" readonly></td>
+                      <td><input type="text" name="" value="material" readonly></td>
+                      <td><input type="text" name="" value="material" readonly></td>
+                  </tr>
+                 
+              </tbody>
+            </table>
+          </div>
+          <div class="col-12 text-center">
+            <button class="btn btn-success">Accept</button>
+            <button class="btn btn-danger">Reject</button>
+          </div>
+        </div>
