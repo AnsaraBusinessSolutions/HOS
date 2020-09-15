d81a470fdeffe738f6abd967d93da3dfc7fcdd4d
Karthikairasu K <karthikairasuk@gmail.com>
2020-09-15 19:29:20 +0530

    Update request_order_details.blade.php

--- resources/views/inbound/request_order_details.blade.php
+++ resources/views/inbound/request_order_details.blade.php
@@ -41,6 +41,30 @@
             <button class="btn btn-danger" data-toggle="modal" data-target="#rejected_reason">Reject</button>
           </div>
         </div>
+        <!-- The Modal -->
+<div class="modal" id="rejected_reason">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Modal Heading</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        Modal body..
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
 
         @stop
 @push('scripts')
