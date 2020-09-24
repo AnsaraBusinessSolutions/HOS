e91f92df0ca9caf6c50e00af4b34f05c3c574ad9
Karthikairasu K <karthikairasuk@gmail.com>
2020-09-24 11:47:55 +0530

    Update store_order.blade.php

--- resources/views/hos/store_order.blade.php
+++ resources/views/hos/store_order.blade.php
@@ -77,7 +77,7 @@
               <div class="col-12 text-center">
                   <table id="store_order" class="table table-striped table-bordered text-center">
                     <thead>
-                        <tr class="bg_color">
+                        <tr class="bg_color ">
                             <!-- <th width="3%" class="px-3">All <input type="checkbox" id="check_all"></th> -->
                             <th width="3%" class="text-nowrap px-3">Item #</th>
                             <th width="15%" class="text-nowrap px-3">NUPCO Material</th>
@@ -89,7 +89,7 @@
                             <th width="8%" class="text-nowrap px-3">Available</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody class="search_data">
                         
                     </tbody>
                   </table>
@@ -133,7 +133,7 @@ $(function() {
       table.row.add( [
             // '<td width="3%" class="text-nowrap px-3"><input type="checkbox" data-row_id ="'+counter+'"></td>',
             '<td class="p-0"><input type="hidden" class="form-control h_1rem" data-row_id ="'+counter+'" data-name="nupco_trade_code" id="nupco_trade_code_'+counter+'" name="nupco_trade_code[]">'+counter+'</td>',
-            '<td class="search_data p-0"><input type="text" class="material_data form-control h_1rem" data-row_id ="'+counter+'" data-name="nupco_generic_code" id="nupco_generic_code_'+counter+'" name="nupco_generic_code[]" maxlength="20" autocomplete="off"><div id="nupco_generic_code_list_'+counter+'" class="position-relative"></div></td>',
+            '<td class="p-0"><input type="text" class="material_data form-control h_1rem" data-row_id ="'+counter+'" data-name="nupco_generic_code" id="nupco_generic_code_'+counter+'" name="nupco_generic_code[]" maxlength="20" autocomplete="off"><div id="nupco_generic_code_list_'+counter+'" class="position-relative"></div></td>',
             '<td class="p-0"><input type="text"  class="material_data form-control h_1rem"  data-row_id ="'+counter+'" data-name="customer_code" id="customer_code_'+counter+'" name="customer_code[]" maxlength="20" autocomplete="off"><div id="customer_code_list_'+counter+'"></div></td>',
             '<td class="p-0"><input type="text"  class="form-control h_1rem"  data-row_id ="'+counter+'" data-name="customer_code_cat" id="customer_code_cat_'+counter+'" name="customer_code_cat[]" readonly><div id="customer_code_cat_list_'+counter+'"></div></td>',
             '<td class="p-0"><input type="text"  class="material_data form-control h_1rem" data-row_id ="'+counter+'" data-name="nupco_desc" id="nupco_desc_'+counter+'" name="nupco_desc[]" autocomplete="off"><div id="nupco_desc_list_'+counter+'"></div></td>',
