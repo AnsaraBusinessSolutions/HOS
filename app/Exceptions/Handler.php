aa51917c48818cb02f14e00cae27b4c77304cb02
Ankita <70564049+abs-ankita-dev@users.noreply.github.com>
2020-10-12 10:07:28 +0530

    login file related chnages

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof \Illuminate\Session\TokenMismatchException) {
+            return redirect()->route('main_page');
+        }
         return parent::render($request, $exception);
     }
 }
