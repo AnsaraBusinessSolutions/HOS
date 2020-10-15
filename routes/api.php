8dacdda049759aa53a7d9793a6fd7dd8715d069d
Ankita <70564049+abs-ankita-dev@users.noreply.github.com>
2020-10-15 18:13:25 +0530

    change api file

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/add_stock', 'HomeController@AddStock')->name('add.stock');
+Route::post('/add_stock', 'HomeController@AddStock');
